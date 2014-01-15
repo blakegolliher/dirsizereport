43aaffff9f78534dac2873b9469e19606cee34df
Blake Golliher <blakegolliher@gmail.com>
2014-01-14 18:08:35 -0800

    Update dirsizereport.py

--- dirsizereport.py
+++ dirsizereport.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-## 
+##
 #
-# Based on Rosetta Code recursive dir walk example 
+# Based on Rosetta Code recursive dir walk example
 # http://rosettacode.org/wiki/Walk_a_directory/Recursively#Python
 #
 # Blake Golliher - blakegolliher@gmail.com
@@ -10,7 +10,6 @@
 
 import fnmatch
 import sys,os
-from math import log
 
 rootPath = sys.argv[1]
 pattern = '*'
@@ -23,20 +22,10 @@ for root, dirs, files in os.walk(rootPath):
         ## print "SIZE = %s  -> %s" % (size,fullpathname)
         fullSize = fullSize + size
 
-## Thanks internet!
-## http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
-unit_list = zip(['bytes', 'kB', 'MB', 'GB', 'TB'], [0, 0, 1, 2, 2])
 def sizeof_fmt(num):
-    """Human friendly file size"""
-    if num > 1:
-        exponent = min(int(log(num, 1024)), len(unit_list) - 1)
-        quotient = float(num) / 1024**exponent
-        unit, num_decimals = unit_list[exponent]
-        format_string = '{:.%sf} {}' % (num_decimals)
-        return format_string.format(quotient, unit)
-    if num == 0:
-        return '0 bytes'
-    if num == 1:
-        return '1 byte'
+    for x in ['Bytes','KB','MB','GB','TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
 
 print "Whole size of path %s is %s." % (rootPath,sizeof_fmt(fullSize))
